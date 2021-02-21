06972fdff4201dd00cf5f18ce7fc554dbaa220c3
javierdarioruizm <javierdarioruizm@gmail.com>
2021-02-21 21:54:40 +0100

    Creación de las variables listaPosts, listaCategorias,  categoriaSelec y seleccionada en archivo blog

--- Practica3BlogAngular/src/app/components/blog/blog.component.ts
+++ Practica3BlogAngular/src/app/components/blog/blog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/post.interface';
 
 @Component({
   selector: 'app-blog',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent implements OnInit {
+
+  listaPosts: Post[];
+  listaCategorias: string[];
+  categoriaSelec: string;
+  seleccionada: boolean;
 
   constructor() { }
 
