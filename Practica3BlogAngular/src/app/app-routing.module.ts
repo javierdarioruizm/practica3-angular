3b2bc9f8fbee514eb72dfac8269001fab9ae3742
javierdarioruizm <javierdarioruizm@gmail.com>
2021-02-21 21:03:33 +0100

    Rutas de los componentes añadidas en el arhivo app-routing

--- Practica3BlogAngular/src/app/app-routing.module.ts
+++ Practica3BlogAngular/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './components/blog/blog.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/blog' },
+  { path: 'blog', component: BlogComponent },
+  { path: 'formulario', component: FormularioComponent },
+  { path: '**', redirectTo: '/blog' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
