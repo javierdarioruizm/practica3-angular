cdbeaeaf5361dcbdb38dee9ea97bb6a32c340db9
javierdarioruizm <javierdarioruizm@gmail.com>
2021-02-21 21:37:08 +0100

    Creación de variables nuevoPost y listaPosts e importación del interface Post en fichero Servicio

--- Practica3BlogAngular/src/app/servicio.service.ts
+++ Practica3BlogAngular/src/app/servicio.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Post } from './post.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicioService {
+
+  nuevoPost: Post;
+  private listaPosts: Post[];
 
   constructor() { }
 }
