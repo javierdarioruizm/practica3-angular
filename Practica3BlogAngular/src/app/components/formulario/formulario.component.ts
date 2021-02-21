76964edce91905ec9cc4b0ccecd8f9c821c15545
javierdarioruizm <javierdarioruizm@gmail.com>
2021-02-21 22:47:19 +0100

    Inyección del servicio en el contructor

--- Practica3BlogAngular/src/app/components/formulario/formulario.component.ts
+++ Practica3BlogAngular/src/app/components/formulario/formulario.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { Post } from 'src/app/post.interface';
+import { ServicioService } from 'src/app/servicio.service';
 
 @Component({
   selector: 'app-formulario',
@@ -12,7 +13,7 @@ export class FormularioComponent implements OnInit {
   listaPosts: Post[];
   formulario: FormGroup;
 
-  constructor() { }
+  constructor(private servicioService: ServicioService) { }
 
   ngOnInit(): void {
   }
